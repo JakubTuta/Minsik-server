c16a3a0962a861072243c4fc3daf4a8043d3beef
JakubTuta <jakubtutka02@gmail.com>
2026-03-04 23:24:24 +0100

    remove view count from books endpoints

--- services/recommendation/app/proto/recommendation_pb2_grpc.py
+++ services/recommendation/app/proto/recommendation_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import recommendation_pb2 as recommendation__pb2
+from . import recommendation_pb2 as recommendation__pb2
 
 GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
